d8976adae19b50f15b2b04883c6a959b23c418d9
uetak330 <uetak330@gmail.com>
2024-11-30 07:18:25 +0900

    insert()の定義
        @Options(useGeneratedKeys = true, keyProperty = "id")
    を付けて自動採番を有効に。
    MyBatisではInsertのアノテーションが付いたメソッドの戻り値はvoidで無ければならないらしい。

--- src/main/java/com/example/todoapi/repository/task/TaskRepository.java
+++ src/main/java/com/example/todoapi/repository/task/TaskRepository.java
@@ -1,14 +1,34 @@
 package com.example.todoapi.repository.task;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.Optional;
 
+/**
+ * タスクに関連するデータベース操作を行うリポジトリインターフェース
+ */
 @Mapper
 public interface TaskRepository {
 
+    /**
+     * 指定されたタスクIDに対応するタスクをデータベースから取得する。<p></p>
+     *
+     * @param taskId 取得するタスクのID
+     * @return タスクが存在する場合は該当する {@link TaskRecord}のOptional,
+     * 存在しない場合は空のOptional
+     * */
     @Select("SELECT id, title FROM tasks WHERE id = #{taskId}")
     Optional<TaskRecord> select(Long taskId);
 
+    /* ➁それを解決するために引数に渡したタスクレコードのIDフィールドに
+    オートインクリメントされた値をセットしてもらう
+    ＠Optionsを使う*/
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("INSERT INTO tasks (title) VALUES (#{title})")
+    void insert(TaskRecord record);
 }
+    /*➀MyBatisではInsertのアノテーションが付いたメソッドの戻り値はvoid出なければいけない。
+      そうするとオートインクリメントされた値を戻り値で取得できない。 */
