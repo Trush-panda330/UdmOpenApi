2b316498d6040afa98d5914d2d866b2268fbc314
uetak330 <uetak330@gmail.com>
2024-11-29 09:08:38 +0900

    schema.sqlとdata.sqlの定義

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into samples(content) values ('Hello, world!!');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+create table samples(
+    id bigint not null primary key auto_increment,
+    content varchar(256) not null
+);
