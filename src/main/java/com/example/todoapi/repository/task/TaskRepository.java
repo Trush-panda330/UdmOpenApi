9ea6ad377c151c16a903afffa706cdedc290cabe
uetak330 <uetak330@gmail.com>
2024-11-30 17:20:15 +0900

    selectList()の定義
    int型のlimitとlong型のoffsetを受け取るようにして、
    MyBatisのSelect文でそれらを使用して定義する

--- src/main/java/com/example/todoapi/repository/task/TaskRepository.java
+++ src/main/java/com/example/todoapi/repository/task/TaskRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -12,6 +13,9 @@ import java.util.Optional;
  */
 @Mapper
 public interface TaskRepository {
+
+    @Select("SELECT id, title FROM tasks LIMIT #{limit} OFFSET #{offset}")
+    List<TaskRecord> selectList(int limit, long offset);
 
     /**
      * 指定されたタスクIDに対応するタスクをデータベースから取得する。<p></p>
@@ -29,6 +33,7 @@ public interface TaskRepository {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @Insert("INSERT INTO tasks (title) VALUES (#{title})")
     void insert(TaskRecord record);
+
 }
     /*➀MyBatisではInsertのアノテーションが付いたメソッドの戻り値はvoid出なければいけない。
       そうするとオートインクリメントされた値を戻り値で取得できない。 */
