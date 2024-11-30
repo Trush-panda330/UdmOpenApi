96631e6f3ec9ce9084fc80c27c9a0a8557e42784
uetak330 <uetak330@gmail.com>
2024-11-30 09:14:46 +0900

    メソッドの切り出し
    ラムダ式のメソッド参照を使うようにメソッドの切り出しを実施

--- src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
+++ src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
@@ -2,6 +2,7 @@ package com.example.todoapi.controller.advice;
 
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.InvalidParam;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.stream.Collectors;
@@ -21,16 +22,18 @@ public class BadRequestErrorCreator {
         //getFieldError()ではなく
         var invalidParamList = ex.getFieldErrors()
                 .stream()
-                .map(fieldError -> {
-                    var invalidParam = new InvalidParam();
-                    invalidParam.setName(fieldError.getField());
-                    invalidParam.setReason(fieldError.getDefaultMessage());
-                    return invalidParam;
-                })
+                .map(BadRequestErrorCreator::apply)
                 .collect(Collectors.toList());
 
         var error = new BadRequestError();
         error.setInvalidParams(invalidParamList);
         return error;
+    }
+
+    private static InvalidParam apply(FieldError fieldError) {
+        var invalidParam = new InvalidParam();
+        invalidParam.setName(fieldError.getField());
+        invalidParam.setReason(fieldError.getDefaultMessage());
+        return invalidParam;
     }
 }
