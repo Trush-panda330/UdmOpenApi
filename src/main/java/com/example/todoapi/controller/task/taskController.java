4b6c57ec4e3edbe096740afe9a9cc5ae000fb8b6
uetak330 <uetak330@gmail.com>
2024-12-01 09:11:51 +0900

    TaskControllerの修正
    editTask()内でupdate()メソッドを使う

--- src/main/java/com/example/todoapi/controller/task/taskController.java
+++ src/main/java/com/example/todoapi/controller/task/taskController.java
@@ -86,9 +86,10 @@ public class taskController implements TasksApi {
         return ResponseEntity.ok(dto);
     }
 
+
     @Override
-    public ResponseEntity<TaskDTO> editTask(Long taskId) {
-        var taskEntity = taskService.find(taskId);
+    public ResponseEntity<TaskDTO> editTask(Long taskId, TaskForm taskForm) {
+        var taskEntity = taskService.update(taskId,taskForm.getTitle());
         var dto = toTaskDTO(taskEntity);
 
         return ResponseEntity.ok(dto);
